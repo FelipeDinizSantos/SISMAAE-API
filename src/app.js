7a992e1d3f8d80c6a035e58d791f91877a015a1b
Felipe <felipedinizdossantos0@gmail.com>
2025-09-16 09:10:46 -0300

    Implementa logs de requisições para o terminal da API.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const auth = require("./middlewares/auth");
+const cors = require("cors");
+const morgan = require('morgan');
 
 const materialRoutes = require("./routes/material.routes");
 const moduloRoutes = require("./routes/modulo.route");
@@ -8,6 +9,8 @@ const batalhoesRoutes = require("./routes/batalhao.routes");
 const usuarioRoutes = require("./routes/usuario.routes");
 
 app.use(express.json());
+app.use(cors());
+app.use(morgan(':method :url :status :response-time ms'));
 
 app.use('/api/', materialRoutes);
 app.use('/api/', moduloRoutes);
