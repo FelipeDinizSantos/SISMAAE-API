f1e236d07235ad4489bf1d64393c91d9902a34b9
Felipe <felipedinizdossantos0@gmail.com>
2026-02-03 10:22:26 -0300

    Criação de materiais para perfis ADMIN e COL

--- src/controllers/material.controller.js
+++ src/controllers/material.controller.js
@@ -1,4 +1,57 @@
 const usuarioServiceFactory = require("../services/usuarios/usuario.service.factory");
+const pool = require("../config/db");
+
+exports.store = async (req, res) => {
+    const { serial_num, nome, status, origem_id, loc_id } = req.body;
+
+    // Validações 
+    if (typeof serial_num !== "string" && serial_num.length > 6) {
+        return res.status(400).json({ error: 'O número de série do material precisa ser do tipo String e conter no máximo 6 caracteres.' })
+    }
+    const nomeValido = ["RADAR", "RBS70", "COAAE"].some(n => n === nome.toUpperCase());
+    if (!nomeValido) {
+        return res.status(400).json({ error: 'Nome de material inválido. Nomes disponíveis: "RADAR", "RBS70", "COAAE"' })
+    }
+    const statusValido = ['DISPONIVEL', 'DISP_C_RESTRICAO', 'INDISPONIVEL', 'MANUTENCAO'].some(s => s === status.toUpperCase());
+    if (!statusValido) {
+        return res.status(400).json({ error: 'Status do material inválido. Status disponíveis: "DISPONIVEL", "DISP_C_RESTRICAO", "INDISPONIVEL", "MANUTENCAO"' });
+    }
+    if ((await pool.query(
+        "SELECT id FROM batalhoes WHERE id = ?",
+        [origem_id]))[0].length === 0) {
+        return res.status(400).json({ error: 'ID da OM de origem informado não existe!' });
+    }
+    if ((await pool.query(
+        "SELECT id FROM batalhoes WHERE id = ?",
+        [loc_id]))[0].length === 0) {
+        return res.status(400).json({ error: 'ID da OM atual informado não existe!' });
+    }
+
+    try {
+        const [resultado] = await pool.query(`
+            INSERT INTO materiais (
+                serial_num,
+                nome,
+                status,
+                origem_id,
+                loc_id
+            ) VALUES (?, ?, ?, ?, ?)
+        `, [serial_num.toUpperCase(), nome.toUpperCase(), status.toUpperCase(), origem_id, loc_id]);
+
+        return res.status(201).json({
+            id: resultado.insertId,
+            serial_num,
+            nome,
+            status,
+            origem_id,
+            loc_id
+        });
+
+    } catch (erro) {
+        console.log("controllers/material: \n" + erro);
+        return res.status(500).json({ error: 'Erro ao criar material.' });
+    }
+}
 
 exports.index = async (req, res) => {
     const usuario = req.usuario;
--- src/routes/material.routes.js
+++ src/routes/material.routes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const materialController = require("../controllers/material.controller");
 const auth = require("../middlewares/auth");
+const checkPerfil = require("../middlewares/checkPerfil");
 
 router.get("/materiais/", auth, materialController.index);
-router.get("/materiais/:id", auth,  materialController.show);
-router.put("/materiais/:id", auth,  materialController.edit);
+router.get("/materiais/:id", auth, materialController.show);
+router.post("/materiais/", auth, checkPerfil([1, 3]), materialController.store);
+router.put("/materiais/:id", auth, materialController.edit);
 
 module.exports = router;
